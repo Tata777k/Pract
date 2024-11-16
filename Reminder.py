3ca4c0848b551c947b4a557f244353d0c7eb9f9a
Tatyana Kos <kosttata777@gmail.com>
2024-11-16 21:30:58 +0300

    add play in music

--- Reminder.py
+++ Reminder.py
@@ -34,6 +34,12 @@ def check():
             t = 0
     window.after(10000, check)
 
+
+def play_snd():
+    pygame.mixer.init()
+    pygame.mixer.music.load("raz.mp3")
+    pygame.mixer.music.play()
+
 window = Tk()
 window.title("Напоминание")
 label = Label(text="Установите напоминание")
