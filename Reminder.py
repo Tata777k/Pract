260ab0b7dd16f5ad66043b38de5760dbdaf21db1
Tatyana Kos <kosttata777@gmail.com>
2024-11-17 00:14:59 +0300

    increase font size, removed seconds

--- Reminder.py
+++ Reminder.py
@@ -17,34 +17,42 @@ def set():
             minute = int(rem.split(":")[1])
             now = datetime.datetime.now()
             print(now)
-            dt = now.replace(hour=hour, minute=minute)
+            dt = now.replace(hour=hour, minute=minute, second=0)
             print(dt)
             t = dt.timestamp()
             print(t)
+            check()
         except Exception as e:
             mb.showerror("Ошибка.", f"Произошла ошибка {e}")
 
 
 def check():
+    print( "check:" )
     global t
     if t:
         now = time.time()
         if now >= t:
             play_snd()
             t = 0
-    window.after(10000, check)
+            print( "finish>>>" )
+        else:
+            print("after>>>")
+            window.after(10000, check)
 
 
 def play_snd():
     pygame.mixer.init()
     pygame.mixer.music.load("raz.mp3")
+    print("playing...")
     pygame.mixer.music.play()
 
 window = Tk()
 window.title("Напоминание")
-label = Label(text="Установите напоминание")
+label = Label(text="Установите напоминание",font=("Arial", 14))
 label.pack(pady=10)
 set_button = Button(text="Установить напоминание", command=set)
 set_button.pack()
+
+#play_snd()
 
 window.mainloop()
